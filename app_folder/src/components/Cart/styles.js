42caff1fb198c75fc2ff227cd1be0ac1207d5513
Vitor Lima <mikomirOO@gmail.com>
2022-03-30 01:44:09 -0300

    Correcting code for newer versions of APIs
    Correcting code for newer versions of APIs for Stripe and CommerceJS

---
+++ app_folder/src/components/Cart/styles.js
@@ -0,0 +1,35 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+  title: {
+    marginTop: '1%',
+    color: "#FFFFFF"
+  },
+  emptyButton: {
+    minWidth: '150px',
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: '5px',
+    },
+    [theme.breakpoints.up('xs')]: {
+      marginRight: '20px',
+    },
+  },
+  checkoutButton: {
+    minWidth: '150px',
+  },
+  link: {
+    textDecoration: 'none',
+    color: "#FFFFFF"
+  },
+  typography: {
+    color: "#FFFFFF"
+  },
+  cardDetails: {
+    color: "#FFFFFF",
+    display: 'flex',
+    padding: '5%',
+    width: '100%',
+    justifyContent: 'center',
+  },
+}));
